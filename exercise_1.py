36a94fb9ba48b2ee7de51881f04979ba8c1770dc
melfernandess <elisa.octaviano@engenharia.ufjf.br>
2024-03-03 21:30:02 -0300

    feat: developed exercises

---
+++ exercise_1.py
@@ -0,0 +1,6 @@
+from helpers import calculate_tsne, extract_embeddings_and_labels, load_data, plot_tsne
+
+data = load_data('mini_gm_public_v0.1.p')
+embeddings, labels = extract_embeddings_and_labels(data)
+embeddings_tsne = calculate_tsne(embeddings)
+plot_tsne(embeddings_tsne, labels)
---
+++ exercise_2.py
@@ -0,0 +1,7 @@
+from helpers import extract_embeddings_and_labels, load_data, perform_kfold_validation, plot_confusion_matrix
+
+data = load_data('mini_gm_public_v0.1.p')
+X, y = extract_embeddings_and_labels(data)
+true_labels, predicted_labels_cosine, predicted_labels_euclidean = perform_kfold_validation(X, y)
+plot_confusion_matrix(true_labels, predicted_labels_cosine, 'Matriz de Confusão para Distância Cosseno')
+plot_confusion_matrix(true_labels, predicted_labels_euclidean, 'Matriz de Confusão para Distância Euclidiana')
